2c9f36fa7898614bdeda932d283fd1ad4f299773
wjdqudwls <wjdqudwls100@gmail.com>
2026-03-25 17:17:03 +0900

    chore: add CQRS skeleton packages

---
+++ src/main/java/com/conk/wms/Team1BackendWmsApplication.java
@@ -0,0 +1,13 @@
+package com.conk.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1BackendWmsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Team1BackendWmsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/conk/wms/Team1BackendWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conk.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1BackendWmsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
